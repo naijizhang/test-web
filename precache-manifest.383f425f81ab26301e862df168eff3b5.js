cd18105fb9093b07103281ed97d10a0aadd549e2
Naiji Zhang <naijizhang@gmail.com>
2019-10-09 13:12:23 -0700

    Updates

---
+++ precache-manifest.383f425f81ab26301e862df168eff3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3847b3c39d6c1f16de6bdb9f74783e75",
+    "url": "/test-web/index.html"
+  },
+  {
+    "revision": "8352bad8dc74551fd0a2",
+    "url": "/test-web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8e8d83c1724cf7be6597",
+    "url": "/test-web/static/js/2.27fdf162.chunk.js"
+  },
+  {
+    "revision": "8352bad8dc74551fd0a2",
+    "url": "/test-web/static/js/main.4a90b3d1.chunk.js"
+  },
+  {
+    "revision": "a7eabb40e82ea6a8c89d",
+    "url": "/test-web/static/js/runtime-main.6f0ef4c1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-web/static/media/logo.25bf045c.svg"
+  }
+]);
